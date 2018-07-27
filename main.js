18b9bb57864fbffe56711ff01abc89816a6c63eb
Mateus Rodrigues Costa <35977076+mateusrodc@users.noreply.github.com>
2018-07-27 10:35:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,31 +3,55 @@ var noticias = [];
 function atualizarLista(noticia) {
     var lista = document.getElementById('noticias-recentes-list');
     var li = document.createElement('li');
+    var data_noticia = new Date(Date.parse(noticia.data));
+    var data_atual = new Date().getTime();
     li.setAttribute('id', 'noticia-' + noticia.id);
     li.setAttribute('class', 'noticia');
-    li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
-        + noticia.titulo
-        + '</p>'
-        + '<p class="conteudo">'
-        + noticia.conteudo
-        + '<br>'
-        + '<span>------------------</span>'
-        + '<br>'
-        + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
-        + '</p>';
-    lista.appendChild(li);
+    if (data_noticia <= data_atual && data_noticia != null){
+        li.innerHTML = '<p class="titulo" onclick="mostrarNoticia(' + noticia.id + ')">'
+            + noticia.titulo
+            + '</p>'
+            + '<p class="conteudo">'
+            + noticia.conteudo
+            + '<br>'
+            + noticia.nome
+            + '<br>'
+            + noticia.email
+            + '<br>'
+            + noticia.data
+            + '<br>'
+            + '<span>------------------</span>'
+            + '<br>'
+            + '<button onclick="ocultarNoticia(' + noticia.id + ')">Fechar</button>';
+            + '</p>';
+        lista.appendChild(li);
+    }
 }
 
 function salvar(form) {
     var titulo = document.getElementById('frm-titulo').value;
     var conteudo = document.getElementById('frm-conteudo').value;
+    var nome = document.getElementById('frm-nome').value;
+    var email = document.getElementById('frm-email').value;
+    var data = document.getElementById('frm-data').value;
     var noticia = {
         id: noticias.length,
         titulo: titulo,
-        conteudo: conteudo
+        conteudo: conteudo,
+        nome: nome,
+        email: email,
+        data: new Date(Date.parse(data))
     };
     noticias.push(noticia);
-    atualizarLista(noticia);
+    noticias.sort((noticia1,noticia2) => noticia2.data-noticia1.data);
+    console.log(noticias)
+    var myList = document.getElementById('noticias-recentes-list');
+    myList.innerHTML = '';
+    
+    for (var i = 0; i < noticias.length; i++){
+        atualizarLista(noticias[i]);
+    }
+    //atualizarLista(noticia);
     form.reset();
 }
 
